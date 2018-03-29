24e883836ced6411cdaa48a016bbd8b5c3bf22f3
Ingmar Drewing <ingmar@drewing.de>
2018-03-30 00:36:15 +0200

    Refactor

--- site.go
+++ site.go
@@ -11,104 +11,127 @@ import (
 // Creates a site dto and the  pages
 // and links being parts of the site in turn
 func NewSiteDto(config staticPersistence.JsonConfig) staticIntf.Site {
+	siteCreator := new(siteCreator)
+	siteCreator.init(config)
+	siteCreator.addConfigData()
+	siteCreator.addPages()
+	siteCreator.addLocations()
+	return siteCreator.site
+}
 
-	site := new(siteDto)
+type siteCreator struct {
+	site   *siteDto
+	config staticPersistence.JsonConfig
+}
 
-	site.twitterHandle = config.Context.TwitterHandle
-	site.topic = config.Context.Topic
-	site.tags = config.Context.Tags
-	site.domain = config.Domain
-	site.cardType = config.Context.CardType
-	site.section = config.Context.Section
-	site.fbPage = config.Context.FbPage
-	site.twitterPage = config.Context.TwitterPage
-	site.rssPath = config.Deploy.RssPath
-	site.rssFilename = config.Deploy.RssFilename
-	site.css = config.Deploy.CssFileName
-	site.disqusId = config.Context.DisqusShortname
-	site.targetDir = config.Deploy.TargetDir
+func (s *siteCreator) init(config staticPersistence.JsonConfig) {
+	s.config = config
+	s.site = new(siteDto)
+}
 
-	site.description = config.DefaultMeta.BlogExcerpt
+func (s *siteCreator) addConfigData() {
+	s.site.twitterHandle = s.config.Context.TwitterHandle
+	s.site.topic = s.config.Context.Topic
+	s.site.tags = s.config.Context.Tags
+	s.site.domain = s.config.Domain
+	s.site.cardType = s.config.Context.CardType
+	s.site.section = s.config.Context.Section
+	s.site.fbPage = s.config.Context.FbPage
+	s.site.twitterPage = s.config.Context.TwitterPage
+	s.site.rssPath = s.config.Deploy.RssPath
+	s.site.rssFilename = s.config.Deploy.RssFilename
+	s.site.css = s.config.Deploy.CssFileName
+	s.site.disqusId = s.config.Context.DisqusShortname
+	s.site.targetDir = s.config.Deploy.TargetDir
+	s.site.description = s.config.DefaultMeta.BlogExcerpt
+}
 
-	if staticUtil.DirExists(config.Src.PostsDir) {
-		dtos := staticPersistence.ReadPagesFromDir(config.Src.PostsDir)
-		for _, dto := range dtos {
-			p := NewPage(dto, config.Domain)
-			newPath := path.Join("/blog/", p.PathFromDocRoot())
-			p.PathFromDocRoot(newPath)
-			site.addPost(p)
-		}
-		bnpg := NewBlogNaviPageGenerator(site, "/blog/")
-		site.postNaviPages = bnpg.Createpages()
-	}
-
-	if staticUtil.DirExists(config.Src.MainPages) {
-		dtos := staticPersistence.ReadPagesFromDir(config.Src.MainPages)
-		for _, dto := range dtos {
-			p := NewPage(dto, config.Domain)
-			site.addMainPage(p)
-		}
-	}
-
-	if staticUtil.DirExists(config.Src.MarginalDir) {
-		dtos := staticPersistence.ReadPagesFromDir(config.Src.MarginalDir)
-		for _, dto := range dtos {
-			p := NewPage(dto, config.Domain)
-			site.addMarginalPage(p)
-		}
-
-		locs := ElementsToLocations(site.Marginals())
-		for _, l := range locs {
-			site.AddMarginal(l)
-		}
-	}
-
-	if staticUtil.DirExists(config.Src.Narrative) {
-		dtos := staticPersistence.ReadPagesFromDir(config.Src.Narrative)
-		for _, dto := range dtos {
-			p := NewPage(dto, config.Domain)
-			site.addNarrativePage(p)
-		}
-	}
-
-	if staticUtil.DirExists(config.Src.NarrativeMarginals) {
-		dtos := staticPersistence.ReadPagesFromDir(
-			config.Src.NarrativeMarginals)
-		for _, dto := range dtos {
-			p := NewPage(dto, config.Domain)
-			site.addNarrativeMarginalPage(p)
-		}
-	}
+func (s *siteCreator) addLocations() {
 
 	// add configured main navigation
-	for _, fl := range config.Context.Header {
+	for _, fl := range s.config.Context.Header {
 		l := NewLocation(
 			fl.ExternalLink,
-			config.Domain,
+			s.config.Domain,
 			fl.Label,
 			"",
 			fl.Path,
 			fl.FileName)
-		site.AddMain(l)
+		s.site.AddMain(l)
 	}
 
 	// add configured marginal navigation
-	for _, fl := range config.Context.Footer {
+	for _, fl := range s.config.Context.Footer {
 		l := NewLocation(
 			fl.ExternalLink,
-			config.Domain,
+			s.config.Domain,
 			fl.Label,
 			"",
 			fl.Path,
 			fl.FileName)
-		site.AddMarginal(l)
+		s.site.AddMarginal(l)
+	}
+}
+
+func (s *siteCreator) addPages() {
+	if staticUtil.DirExists(s.config.Src.PostsDir) {
+		dtos := staticPersistence.ReadPagesFromDir(s.config.Src.PostsDir)
+		for _, dto := range dtos {
+			p := NewPage(dto, s.config.Domain)
+			newPath := path.Join("/blog/", p.PathFromDocRoot())
+			p.PathFromDocRoot(newPath)
+			s.site.addPost(p)
+		}
+		bnpg := NewBlogNaviPageGenerator(s.site, "/blog/")
+		s.site.postNaviPages = bnpg.Createpages()
 	}
 
-	return site
+	if staticUtil.DirExists(s.config.Src.MainPages) {
+		dtos := staticPersistence.ReadPagesFromDir(s.config.Src.MainPages)
+		for _, dto := range dtos {
+			p := NewPage(dto, s.config.Domain)
+			s.site.addMainPage(p)
+		}
+	}
+
+	if staticUtil.DirExists(s.config.Src.MarginalDir) {
+		dtos := staticPersistence.ReadPagesFromDir(s.config.Src.MarginalDir)
+		for _, dto := range dtos {
+			p := NewPage(dto, s.config.Domain)
+			s.site.addMarginalPage(p)
+		}
+
+		locs := ElementsToLocations(s.site.Marginals())
+		for _, l := range locs {
+			s.site.AddMarginal(l)
+		}
+	}
+
+	if staticUtil.DirExists(s.config.Src.Narrative) {
+		dtos := staticPersistence.ReadPagesFromDir(s.config.Src.Narrative)
+		for _, dto := range dtos {
+			p := NewPage(dto, s.config.Domain)
+			s.site.addNarrativePage(p)
+		}
+	}
+
+	if staticUtil.DirExists(s.config.Src.NarrativeMarginals) {
+		dtos := staticPersistence.ReadPagesFromDir(
+			s.config.Src.NarrativeMarginals)
+		for _, dto := range dtos {
+			p := NewPage(dto, s.config.Domain)
+			s.site.addNarrativeMarginalPage(p)
+		}
+	}
 }
 
 type siteDto struct {
-	main, marginal         []staticIntf.Location
+	pagesContainer
+	locationsContainer
+	configContainer
+}
+
+type pagesContainer struct {
 	posts                  []staticIntf.Page
 	postNaviPages          []staticIntf.Page
 	mainPages              []staticIntf.Page
@@ -116,7 +139,79 @@ type siteDto struct {
 	narrativePages         []staticIntf.Page
 	narrativeMarginalPages []staticIntf.Page
 	narrativeArchivePages  []staticIntf.Page
+}
 
+func (c *pagesContainer) Posts() []staticIntf.Page {
+	return c.posts
+}
+
+func (c *pagesContainer) PostNaviPages() []staticIntf.Page {
+	return c.postNaviPages
+}
+
+func (c *pagesContainer) Pages() []staticIntf.Page {
+	return c.mainPages
+}
+
+func (c *pagesContainer) Marginals() []staticIntf.Page {
+	return c.marginalPages
+}
+
+func (c *pagesContainer) Narratives() []staticIntf.Page {
+	return c.narrativePages
+}
+
+func (c *pagesContainer) NarrativeMarginals() []staticIntf.Page {
+	return c.narrativeMarginalPages
+}
+
+func (c *pagesContainer) addMainPage(p staticIntf.Page) {
+	c.mainPages = append(c.mainPages, p)
+}
+
+func (c *pagesContainer) addMarginalPage(p staticIntf.Page) {
+	c.marginalPages = append(c.marginalPages, p)
+}
+
+func (c *pagesContainer) addPost(p staticIntf.Page) {
+	c.posts = append(c.posts, p)
+}
+
+func (c *pagesContainer) addNarrativePage(p staticIntf.Page) {
+	c.narrativePages = append(c.narrativePages, p)
+}
+
+func (c *pagesContainer) addNarrativeMarginalPage(p staticIntf.Page) {
+	c.narrativeMarginalPages = append(c.narrativeMarginalPages, p)
+}
+
+type locationsContainer struct {
+	main, marginal []staticIntf.Location
+}
+
+func (c *locationsContainer) AddMain(loc staticIntf.Location) {
+	c.add(&c.main, loc)
+}
+
+func (c *locationsContainer) Main() []staticIntf.Location {
+	return c.main
+}
+
+func (c *locationsContainer) AddMarginal(loc staticIntf.Location) {
+	c.add(&c.marginal, loc)
+}
+
+func (c *locationsContainer) Marginal() []staticIntf.Location {
+	return c.marginal
+}
+
+func (c *locationsContainer) add(collection *[]staticIntf.Location, locs ...staticIntf.Location) {
+	for _, l := range locs {
+		*collection = append(*collection, l)
+	}
+}
+
+type configContainer struct {
 	twitterHandle string
 	topic         string
 	tags          string
@@ -133,98 +228,32 @@ type siteDto struct {
 	description   string
 }
 
-func (c *siteDto) Posts() []staticIntf.Page {
-	return c.posts
-}
+func (s *configContainer) TwitterHandle() string { return s.twitterHandle }
 
-func (c *siteDto) PostNaviPages() []staticIntf.Page {
-	return c.postNaviPages
-}
+func (s *configContainer) Description() string { return s.description }
 
-func (c *siteDto) Pages() []staticIntf.Page {
-	return c.mainPages
-}
+func (s *configContainer) Topic() string { return s.topic }
 
-func (c *siteDto) Marginals() []staticIntf.Page {
-	return c.marginalPages
-}
+func (s *configContainer) Tags() string { return s.tags }
 
-func (c *siteDto) Narratives() []staticIntf.Page {
-	return c.narrativePages
-}
+func (s *configContainer) Site() string { return s.domain }
 
-func (c *siteDto) NarrativeMarginals() []staticIntf.Page {
-	return c.narrativeMarginalPages
-}
+func (s *configContainer) CardType() string { return s.cardType }
 
-func (c *siteDto) addMainPage(p staticIntf.Page) {
-	c.mainPages = append(c.mainPages, p)
-}
+func (s *configContainer) Section() string { return s.section }
 
-func (c *siteDto) addMarginalPage(p staticIntf.Page) {
-	c.marginalPages = append(c.marginalPages, p)
-}
+func (s *configContainer) FBPage() string { return s.fbPage }
 
-func (c *siteDto) addPost(p staticIntf.Page) {
-	c.posts = append(c.posts, p)
-}
+func (s *configContainer) TwitterPage() string { return s.twitterPage }
 
-func (c *siteDto) addNarrativePage(p staticIntf.Page) {
-	c.narrativePages = append(c.narrativePages, p)
-}
+func (s *configContainer) RssPath() string { return s.rssPath }
 
-func (c *siteDto) addNarrativeMarginalPage(p staticIntf.Page) {
-	c.narrativeMarginalPages = append(c.narrativeMarginalPages, p)
-}
+func (s *configContainer) RssFilename() string { return s.rssFilename }
 
-func (c *siteDto) AddMain(loc staticIntf.Location) {
-	c.add(&c.main, loc)
-}
+func (s *configContainer) Css() string { return s.css }
 
-func (c *siteDto) Main() []staticIntf.Location {
-	return c.main
-}
+func (s *configContainer) Domain() string { return s.domain }
 
-func (c *siteDto) AddMarginal(loc staticIntf.Location) {
-	c.add(&c.marginal, loc)
-}
+func (s *configContainer) DisqusId() string { return s.disqusId }
 
-func (c *siteDto) Marginal() []staticIntf.Location {
-	return c.marginal
-}
-
-func (c *siteDto) add(collection *[]staticIntf.Location, locs ...staticIntf.Location) {
-	for _, l := range locs {
-		*collection = append(*collection, l)
-	}
-}
-
-func (s *siteDto) TwitterHandle() string { return s.twitterHandle }
-
-func (s *siteDto) Description() string { return s.description }
-
-func (s *siteDto) Topic() string { return s.topic }
-
-func (s *siteDto) Tags() string { return s.tags }
-
-func (s *siteDto) Site() string { return s.domain }
-
-func (s *siteDto) CardType() string { return s.cardType }
-
-func (s *siteDto) Section() string { return s.section }
-
-func (s *siteDto) FBPage() string { return s.fbPage }
-
-func (s *siteDto) TwitterPage() string { return s.twitterPage }
-
-func (s *siteDto) RssPath() string { return s.rssPath }
-
-func (s *siteDto) RssFilename() string { return s.rssFilename }
-
-func (s *siteDto) Css() string { return s.css }
-
-func (s *siteDto) Domain() string { return s.domain }
-
-func (s *siteDto) DisqusId() string { return s.disqusId }
-
-func (s *siteDto) TargetDir() string { return s.targetDir }
+func (s *configContainer) TargetDir() string { return s.targetDir }
