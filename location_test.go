d095d896be48e09f22f7158d20d7593dcf62c063
Ingmar Drewing <ingmar@drewing.de>
2018-04-02 17:41:24 +0200

    Add tests

--- location_test.go
+++ location_test.go
@@ -3,7 +3,7 @@ package staticModel
 import "testing"
 
 func TestNewLocation(t *testing.T) {
-	l := NewLocation("https://example.com", "example.com", "test-title", "thumbnailUrl", "pathFromDocRoot", "filename")
+	l := getLocation()
 
 	expected := "https://example.com"
 	actual := l.extLink
@@ -11,4 +11,80 @@ func TestNewLocation(t *testing.T) {
 	if actual != expected {
 		t.Error("Expected", expected, "but got", actual)
 	}
+}
+
+func TestPathFromDocRootWithName(t *testing.T) {
+	l := getLocation()
+
+	expected := "/pathFromDocRoot/filename"
+	actual := l.PathFromDocRootWithName()
+
+	if actual != expected {
+		t.Error("Expected", expected, "but got", actual)
+	}
+}
+
+func TestUrl(t *testing.T) {
+	l := getLocation()
+
+	expected := "https://example.com/pathFromDocRoot/filename"
+	actual := l.Url()
+
+	if actual != expected {
+		t.Error("Expected", expected, "but got", actual)
+	}
+}
+
+func TestDomain(t *testing.T) {
+	l := getLocation()
+
+	l.Domain("testDomain")
+
+	if l.Domain() != "testDomain" {
+		t.Error("Expected loc to have the newly set value in domain field.")
+	}
+}
+
+func TestHtmlFilename(t *testing.T) {
+	l := getLocation()
+
+	l.HtmlFilename("testHtmlFilename")
+
+	if l.HtmlFilename() != "testHtmlFilename" {
+		t.Error("Expected loc to have the newly set value in htmlFilename field.")
+	}
+}
+
+func TestTitle(t *testing.T) {
+	l := getLocation()
+
+	l.Title("testTitle")
+
+	if l.Title() != "testTitle" {
+		t.Error("Expected loc to have the newly set value in title.")
+	}
+}
+
+func TestThumbnailUrl(t *testing.T) {
+	l := getLocation()
+
+	l.ThumbnailUrl("testUrl")
+
+	if l.ThumbnailUrl() != "testUrl" {
+		t.Error("Expected loc to have the newly set value in thumbnailUrl.")
+	}
+}
+
+func TestExternalLinks(t *testing.T) {
+	l := getLocation()
+
+	l.ExternalLink("externalLink")
+
+	if l.ExternalLink() != "externalLink" {
+		t.Error("Expected loc to have the newly set value in externalLink.")
+	}
+}
+
+func getLocation() *loc {
+	return NewLocation("https://example.com", "example.com", "test-title", "thumbnailUrl", "pathFromDocRoot", "filename")
 }
--- site_test.go
+++ site_test.go
@@ -144,3 +144,63 @@ func getSiteDto() *siteDto {
 	config := staticPersistence.ReadConfig("testResources", "configNew.json")
 	return NewSiteDto(config[0]).(*siteDto)
 }
+
+func TestConfigContainer(t *testing.T) {
+	c := &configContainer{"twitterHandle", "topic", "tags", "cardType", "section", "fbPage", "twitterPage", "rssPath", "rssFilename", "css", "domain", "disqusId", "targetDir", "description"}
+
+	if c.TwitterHandle() != "twitterHandle" {
+		t.Error("configCointainer returning wrong value on TwitterHandle()")
+	}
+
+	if c.Description() != "description" {
+		t.Error("configCointainer returning wrong value on Desription()")
+	}
+
+	if c.Topic() != "topic" {
+		t.Error("configCointainer returning wrong value on Topic()")
+	}
+
+	if c.Tags() != "tags" {
+		t.Error("configCointainer returning wrong value on Tags()")
+	}
+
+	if c.Site() != "domain" {
+		t.Error("configCointainer returning wrong value on Site()")
+	}
+
+	if c.CardType() != "cardType" {
+		t.Error("configCointainer returning wrong value on CardType()")
+	}
+
+	if c.Section() != "section" {
+		t.Error("configCointainer returning wrong value on Section()")
+	}
+
+	if c.FBPage() != "fbPage" {
+		t.Error("configCointainer returning wrong value on FBPage()")
+	}
+
+	if c.TwitterPage() != "twitterPage" {
+		t.Error("configCointainer returning wrong value on TwitterPage()")
+	}
+
+	if c.RssPath() != "rssPath" {
+		t.Error("configCointainer returning wrong value on RssPath()")
+	}
+
+	if c.RssFilename() != "rssFilename" {
+		t.Error("configCointainer returning wrong value on RssFilename()")
+	}
+
+	if c.Css() != "css" {
+		t.Error("configCointainer returning wrong value on Css()")
+	}
+
+	if c.DisqusId() != "disqusId" {
+		t.Error("configCointainer returning wrong value on DisqusId()")
+	}
+
+	if c.TargetDir() != "targetDir" {
+		t.Error("configCointainer returning wrong value on TargetDir()")
+	}
+}
