e2bb9206eba6cd4cc840a9893e352adcd3575fce
Ingmar Drewing <ingmar@drewing.de>
2019-01-14 21:19:47 +0100

    Simplify model

--- pagesContainer.go
+++ pagesContainer.go
@@ -1,6 +1,9 @@
 package staticModel
 
-import "github.com/ingmardrewing/staticIntf"
+import (
+	"github.com/ingmardrewing/staticIntf"
+	"github.com/ingmardrewing/staticUtil"
+)
 
 func NewPagesContainer(variant, headline string) staticIntf.PagesContainer {
 	pc := new(pagesContainer)
@@ -20,93 +23,6 @@ type pagesContainer struct {
 	representationals []staticIntf.Page
 }
 
-func (c *pagesContainer) getIndexOfPage(p staticIntf.Page) int {
-	return c.getIndex(c.pages, p)
-}
-
-func (c *pagesContainer) getIndexOfNaviPage(p staticIntf.Page) int {
-	return c.getIndex(c.naviPages, p)
-}
-
-func (c *pagesContainer) getIndex(
-	pgs []staticIntf.Page,
-	p staticIntf.Page) int {
-
-	for i, l := range pgs {
-		if l.Link() == p.Link() {
-			return i
-		}
-	}
-	return -1
-}
-
-func (c *pagesContainer) GetLastPage(p staticIntf.Page) staticIntf.Page {
-	if c.getIndexOfPage(p) != -1 {
-		return c.getLastOf(c.pages)
-	} else if c.getIndexOfNaviPage(p) != -1 {
-		return c.getLastOf(c.naviPages)
-	}
-	return nil
-}
-
-func (c *pagesContainer) getLastOf(pgs []staticIntf.Page) staticIntf.Page {
-	if len(pgs) > 0 {
-		return pgs[len(pgs)-1]
-	}
-	return nil
-}
-
-func (c *pagesContainer) GetFirstPage(p staticIntf.Page) staticIntf.Page {
-	if c.getIndexOfPage(p) != -1 {
-		return c.getFirstOf(c.pages)
-	} else if c.getIndexOfNaviPage(p) != -1 {
-		return c.getFirstOf(c.naviPages)
-	}
-	return nil
-}
-
-func (c *pagesContainer) getFirstOf(pgs []staticIntf.Page) staticIntf.Page {
-
-	if len(pgs) > 0 {
-		return pgs[0]
-	}
-	return nil
-}
-
-func (c *pagesContainer) GetPageBefore(p staticIntf.Page) staticIntf.Page {
-	if c.getIndexOfPage(p) != -1 {
-		return c.pageBefore(c.pages, p)
-	} else if c.getIndexOfNaviPage(p) != -1 {
-		return c.pageBefore(c.naviPages, p)
-	}
-	return nil
-}
-
-func (c *pagesContainer) pageBefore(pgs []staticIntf.Page, p staticIntf.Page) staticIntf.Page {
-	i := c.getIndex(pgs, p)
-	if i > 0 {
-		return pgs[i-1]
-	}
-	return nil
-}
-
-func (c *pagesContainer) GetPageAfter(p staticIntf.Page) staticIntf.Page {
-	if c.getIndexOfPage(p) != -1 {
-		return c.pageAfter(c.pages, p)
-	} else if c.getIndexOfNaviPage(p) != -1 {
-		return c.pageAfter(c.naviPages, p)
-	}
-	return nil
-}
-
-func (c *pagesContainer) pageAfter(pgs []staticIntf.Page, p staticIntf.Page) staticIntf.Page {
-	i := c.getIndex(pgs, p)
-	if i < len(pgs)-1 {
-		return pgs[i+1]
-	}
-	return nil
-}
-
 func (c *pagesContainer) Variant() string {
 	return c.variant
 }
@@ -117,15 +33,6 @@ func (c *pagesContainer) Headline() string {
 
 func (c *pagesContainer) Representationals() []staticIntf.Page {
 	return c.representationals
-}
-
-func (c *pagesContainer) SiblingPages(p staticIntf.Page) []staticIntf.Page {
-	if c.getIndexOfPage(p) != -1 {
-		return c.pages
-	} else if c.getIndexOfNaviPage(p) != -1 {
-		return c.naviPages
-	}
-	return nil
 }
 
 func (c *pagesContainer) Pages() []staticIntf.Page {
@@ -141,15 +48,15 @@ func (c *pagesContainer) AddRepresentational(p staticIntf.Page) {
 }
 
 func (c *pagesContainer) AddPage(p staticIntf.Page) {
-	if c.getIndexOfPage(p) == -1 {
+	tool := staticUtil.NewPagesContainerTool(c)
+	if tool.GetIndexOfPage(p) == -1 {
 		c.pages = append(c.pages, p)
-		p.Container(c)
 	}
 }
 
 func (c *pagesContainer) AddNaviPage(p staticIntf.Page) {
-	if c.getIndexOfNaviPage(p) == -1 {
+	tool := staticUtil.NewPagesContainerTool(c)
+	if tool.GetIndexOfNaviPage(p) == -1 {
 		c.naviPages = append(c.naviPages, p)
-		p.Container(c)
 	}
 }
